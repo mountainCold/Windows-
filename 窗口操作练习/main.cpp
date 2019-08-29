a8edabaebbc852b99e93ce7d4098c677273fa0e8
cold <mountaincold24@gmail.com>
2019-08-29 08:26:44 +0800

    窗口的控件模态和资源的使用。

--- 窗口操作练习/main.cpp
+++ 窗口操作练习/main.cpp
@@ -1,16 +1,143 @@
 #include<windows.h>
+#include "resource.h"
+INT_PTR CALLBACK DiaLProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+	{
+		SendDlgItemMessage(hWnd, IDC_RADIO3, BM_SETCHECK, BST_CHECKED, 0);
+		SendDlgItemMessage(hWnd, IDC_CHECK1, BM_SETCHECK, BST_CHECKED, 0);
+		return true;
+	}break;
+	case WM_COMMAND:
+	{
+		switch (LOWORD(wParam))
+		{
+		case IDC_BUTTON1:
+		{
+			for (int i=0;i<3;++i)
+			{
+				HWND hRad = GetDlgItem(hWnd, IDC_RADIO1 + i);
+				BOOL Check = SendMessage(hRad, BM_GETCHECK, 0, 0);
+				if (Check)
+				{
+					CHAR buff[100] = { 0 };
+					GetDlgItemTextA(hWnd, IDC_RADIO1 + i, buff, 100);
+					MessageBoxA(hWnd, buff, buff, MB_OK);
+					break;
+				}
+			}
+			return true;
+		}
+		case IDC_BUTTON2:
+		{
+			for (int i = 0; i < 3; ++i)
+			{
+				HWND hCHE = GetDlgItem(hWnd, IDC_CHECK1 + i);
+				BOOL Checked = SendMessage(hCHE, BM_GETCHECK, 0, 0);
+				// 如果被选中就输出
+				if (Checked)
+				{
+					// 获取控件保存的文字内容
+					CHAR Buffer[100] = { 0 };
+					GetDlgItemTextA(hWnd, IDC_CHECK1 + i, Buffer, 100);
+
+					// 显示
+					MessageBoxA(hWnd, Buffer, Buffer, MB_OK);
+				}
+			}
+			return true;
+		}
+		default:
+			break;;
+		}
+	}break;
+	case WM_CLOSE:
+	{
+		EndDialog(hWnd, 0);
+	}break;
+	default:
+		break;
+		//return false;
+	}
+	return false;
+}
+LRESULT CALLBACK WinProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
 LRESULT CALLBACK WinPro(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+	HDC hdc;		//环境句柄
+	PAINTSTRUCT ps; //绘制结构
+	RECT rect;		//矩形结构
 	//获取当前的实例句柄
 	static HINSTANCE hInstance = (HINSTANCE)GetModuleHandle(NULL);
 	switch (uMsg)
 	{
 	case WM_CREATE:
 	{
-		CreateWindow(L"Button", L"点击按钮", WS_CHILD | WS_VISIBLE, 10, 10, 150, 50, 
-			hWnd, (HMENU)0x1000, hInstance, NULL);
-		CreateWindow(L"Button", L"切换父窗口", WS_VISIBLE | WS_CHILD,10,10, 120, 50, hWnd, (HMENU)0x1001, hInstance, NULL);
+		CreateWindow(L"Button", L"弹出对话框", WS_CHILD | WS_VISIBLE, 40, 40, 120, 40, hWnd, 
+			(HMENU)0x1000, hInstance, NULL);
+		CreateWindow(L"Button", L"设置菜单", WS_CHILD | WS_VISIBLE, 40, 100, 120, 40, hWnd, 
+			(HMENU)0x1001, hInstance, NULL); 
+		CreateWindow(L"Button", L"设置图标", WS_CHILD | WS_VISIBLE, 40, 160, 120, 40, hWnd,
+			(HMENU)0x1002, hInstance, NULL);
+		return 0;
 	}break;
+	case WM_COMMAND:
+	{
+		switch (LOWORD(wParam))
+		{
+		case 0x1000:
+		{
+			//模态对话框
+			DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), hWnd, DiaLProc);
+			return 0;
+		}break;
+		case 0x1001:
+		{
+			HMENU hMenu = LoadMenu(hInstance, MAKEINTRESOURCE(IDR_MENU1));
+			SetMenu(hWnd, hMenu);
+			return 0;
+		}
+		case 0x1002:
+		{
+			//加载自定义图标 并修改
+			HICON hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1));
+			SetClassLong(hWnd, GCL_HICON, (LONG)hIcon);
+			return 0;
+		}
+		default:
+			break;
+		}
+		
+	}break;
+	case WM_RBUTTONDOWN:
+	{
+		POINT point = { LOWORD(lParam), HIWORD(lParam) };		ClientToScreen(hWnd, &point);
+		HMENU hMenu = LoadMenu(hInstance, MAKEINTRESOURCE(IDR_MENU1));
+		HMENU hSubMenu = GetSubMenu(hMenu,0);
+		//弹出菜单
+		TrackPopupMenu(hSubMenu,TPM_LEFTALIGN,point.x,point.y,NULL,hWnd,NULL);
+		return 0;
+	}  break;
+	case WM_PAINT:
+	{
+		hdc = BeginPaint(hWnd, &ps);
+		GetClientRect(hWnd, &rect);
+		DrawText(hdc, L"艾欧尼亚，昂扬不灭！", -1, &rect, DT_SINGLELINE |
+			DT_CENTER | DT_VCENTER);
+		EndPaint(hWnd, &ps);
+		return 0;
+	}break;
+	case WM_CLOSE:
+	{
+		PostQuitMessage(0);
+		return 0;
+	}
 	default:
 		break;
 	}
@@ -24,12 +151,15 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance,
 	//创建并注册窗口类
 	WNDCLASS WndClass = { 0 };
 	WndClass.lpfnWndProc = WinPro;
+	//加载自定义光标 NULL 表示使用系统的
+	WndClass.hCursor = LoadCursor(hInstance,MAKEINTRESOURCE(IDC_CURSOR1));
 	WndClass.lpszClassName = L"MYClass";
 	WndClass.hbrBackground = CreateSolidBrush(RGB(255, 255, 255));
 	RegisterClass(&WndClass);
 	//创建窗口并显示更新
 	HWND hWnd = CreateWindow(L"MYClass", L"主窗口", WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+
 	ShowWindow(hWnd, SW_SHOWNORMAL);
 	UpdateWindow(hWnd);
 	//建立消息循环
---
+++ 窗口操作练习/resource.h
@@ -0,0 +1,31 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 窗口操作练习.rc 使用
+//
+#define IDD_DIALOG1                     101
+#define IDC_CURSOR1                     103
+#define IDI_ICON1                       104
+#define IDR_MENU1                       105
+#define IDC_RADIO1                      1002
+#define IDC_RADIO2                      1003
+#define IDC_RADIO3                      1004
+#define IDC_BUTTON1                     1005
+#define IDC_CHECK1                      1006
+#define IDC_CHECK2                      1007
+#define IDC_CHECK3                      1008
+#define IDC_BUTTON2                     1009
+#define ID_40001                        40001
+#define ID_40002                        40002
+#define ID_2                            40003
+#define ID_3                            40004
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_COMMAND_VALUE         40009
+#define _APS_NEXT_CONTROL_VALUE         1010
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
